28c80d12bcdcb47a61d139b32c286cf429d2278a
Soumyadeep Shome <soumyadeepshome99@gmail.com>
2023-05-24 14:44:05 +0530

    24-05-23
    Basics covered

--- README.md
+++ README.md
@@ -1,14 +1,204 @@
 # Python tuitorial
  
-## Basics  [ bascics.py ]
+## Basics  [ bascics.py ](basics.py)
 
-Literals / Datatypes
-    |
-    |-- Integer => int => int()
-    |
-    |-- Float => float => float()
-    |
-    |-- String => str => str()
-    |
-    |-- Boolean => bool => bool()
+Literals / Datatypes<br>
+|-- Integer => int => int()<br>
+|-- Float => float => float()<br>
+|-- String => str => str()<br>
+|-- Boolean => bool => bool()<br>
 
+### print()
+<p>
+-using end and sep </br>
+-using format </br>
+-using f-string </br>
+-using % </br>
+</p>
+
+### input()
+<p>
+-Using input() </br>
+-Using eval() </br>
+</p>
+
+### Operators
+<p>
+-Arithmetic Operators </br>
+-Assignment Operators </br>
+-Comparison Operators </br>
+-Logical Operators </br>
+-Identity Operators </br>
+-Membership Operators </br>
+-Bitwise Operators </br>
+</p>
+
+### Control Flow
+<p>
+-If-else </br>
+-For </br>
+-While </br>
+-Break </br>
+-Continue </br>
+-Pass </br>
+</p>
+
+### range()
+<p>
+-Using range() </br>
+-Using range() with step </br>
+-Using range() with start, stop and step </br>
+</p>
+
+### List Comprehension
+<p>
+-Using List Comprehension </br>
+-Using List Comprehension with if </br>
+-Using List Comprehension with if-else </br>
+-Using List Comprehension with nested if </br>
+-Using List Comprehension with nested if-else </br>
+</p>
+
+
+
+## DSA and OOPs [ intermediate.py ](intermediate.py)
+
+### Data Structures
+<p>
+-List </br>
+-Tuple </br>
+-Set </br>
+-Dictionary </br>
+</p>
+
+### Functions
+<p>
+-Function </br>
+-Function Arguments </br>
+-Default Arguments </br>
+-Keyword Arguments </br>
+-Variable Length Arguments </br>
+-Anonymous Function </br>
+-Recursion </br>
+</p>
+
+### Classes and Objects
+<p>
+-Class </br>
+-Object </br>
+-Constructor </br>
+-Self </br>
+-Instance Variables </br>
+-Class Variables </br>
+-Static Methods </br>
+-Inheritance </br>
+-Overriding Methods </br>
+-Data Hiding </br>
+</p>
+
+### Modules
+<p>
+-Importing Modules </br>
+-Importing from Modules </br>
+-Importing Specific Items </br>
+-Importing with Alias </br>
+-Importing Everything </br>
+</p>
+
+### File Handling
+<p>
+-Opening a File </br>
+-Reading a File </br>
+-Writing to a File </br>
+-Appending to a File </br>
+-Closing a File </br>
+</p>
+
+### Exception Handling
+<p>
+-Try </br>
+-Except </br>
+-Finally </br>
+-Raise </br>
+-Assert </br>
+</p>
+
+
+
+### Regular Expressions
+<p>
+-Match </br>
+-Search </br>
+-Sub </br>
+-Subn </br>
+-Group </br>
+-Flags </br>
+</p>
+
+### Multithreading
+<p>
+-Thread </br>
+-Starting a Thread </br>
+-Starting a Thread with Arguments </br>
+-Starting a Thread with Keyword Arguments </br>
+-Starting a Thread with Both Arguments and Keyword Arguments </br>
+</p>
+
+### Networking
+<p>
+-Client </br>
+-Server </br>
+</p>
+
+### Database
+<p>
+-Connecting to a Database </br>
+-Creating a Table </br>
+-Inserting Data </br>
+-Selecting Data </br>
+-Updating Data </br>
+-Deleting Data </br>
+</p>
+
+### GUI
+<p>
+-Label </br>
+-Button </br>
+-Entry </br>
+-Text </br>
+-Frame </br>
+-Checkbutton </br>
+-Radiobutton </br>
+-Listbox </br>
+-Menu </br>
+-Message </br>
+-OptionMenu </br>
+-Scale </br>
+-Scrollbar </br>
+-Toplevel </br>
+</p>
+
+### Web Scraping
+<p>
+-BeautifulSoup </br>
+-Scraping a Webpage </br>
+-Scraping Multiple Webpages </br>
+</p>
+
+### Sending Emails
+<p>
+-SMTP </br>
+-Sending a Plain Text Email </br>
+-Sending an HTML Email </br>
+-Sending an Email with Attachment </br>
+</p>
+
+### Python - MySQL
+<p>
+-Connecting to MySQL </br>
+-Creating a Table </br>
+-Inserting Data </br>
+-Selecting Data </br>
+-Updating Data </br>
+-Deleting Data </br>
+</p>
--- basics.py
+++ basics.py
@@ -1,4 +1,4 @@
- #print hello world
+#print hello world
 print("Hello World")
 
 #declare variable  of each type
@@ -13,13 +13,14 @@ print(type(y))
 print(type(z))
 print(type(a))
 
-
 #convert variable to other type
 print(float(x))
 print(int(y))
 print(int(z))
 print(str(x))
 print(bool(x))
+
+#------------------------------------------------------------------------
 
 #print statement using format{}
 print("x is {} and y is {}".format(x,y))
@@ -32,8 +33,8 @@ print("x is %d and y is %f"%(x,y))
 
 #print statement with multiple lines
 print("""this is a
-multiline
-statement""")
+        multiline
+        statement""")
 print("this is a \nmultiline \nstatement")
 
 #print statement with end
@@ -41,13 +42,135 @@ print("this is a", end=" ")
 
 print("single line statement")
 
+#------------------------------------------------------------------------
 
 #take a inout from the user and print it
 x = input("Enter a number: ")
 print(x)
 
-#decalre a funtion to find whether a number is pallindrome or not
-def is_pallindrome(x):
-    return str(x) == str(x)[::-1]
+#take a inout from the user and print it using eval()
+x = eval(input("Enter a number: "))
+print(x)
 
-print(is_pallindrome(121))
+#------------------------------------------------------------------------
+
+#example of each artithmatic operator
+print(2+3) #addition (5) 
+print(2-3) #subtraction (-1)
+print(2*3) #multiplication (6)
+print(2/3) #division (0.6666666666666666)
+print(2//3) #floor division (0)
+print(2**3) #exponent (8)
+print(2%3) #modulus (2)
+
+#example of each assignment operator
+x = 1 #equal to
+x += 1 #addition
+x -= 1 #subtraction
+x *= 1 #multiplication
+x /= 1 #division
+x //= 1 #floor division
+x **= 1 #exponent
+x %= 1 #modulus
+
+#example of each comparison operator
+print(2==3) #equal to (false)
+print(2!=3) #not equal to (true)
+print(2>3) #greater than (false)
+print(2<3) #less than (true)
+print(2>=3) #greater than or equal to  (false)
+print(2<=3) #less than or equal to (true)
+
+#example of each logical operator
+print(2==3) #equal to (false)
+print(2!=3) #not equal to (true)
+print(2>3) #greater than (false)
+print(2<3) #less than (true)
+print(2>=3) #greater than or equal to  (false)
+print(2<=3) #less than or equal to (true)
+
+#example of each identity operator
+print(2 is 3) #is (false)
+print(2 is not 3) #is not (true)
+
+#example of each membership operator
+print(2 in [1,2,3]) #in (true)
+print(2 not in [1,2,3]) #not in (false)
+
+#example of each bitwise operator
+print(2&3) #bitwise and (2)
+print(2|3) #bitwise or (3)
+print(2^3) #bitwise xor (1)
+print(~2) #bitwise not (-3)
+print(2<<3) #bitwise left shift (16)
+print(2>>3) #bitwise right shift (0)
+
+#------------------------------------------------------------------------
+
+#example of if-else statement
+if 2==3:
+    print("2 is equal to 3")
+else:
+    print("2 is not equal to 3")
+
+#example of if-elif-else statement
+if 2==3:
+    print("2 is equal to 3")
+elif 2==2:
+    print("2 is equal to 2")
+else:
+    print("2 is not equal to 3")
+
+#example of nested if-else statement
+if 2==3:
+    print("2 is equal to 3")
+else:
+    if 2==2:
+        print("2 is equal to 2")
+    else:
+        print("2 is not equal to 3")
+
+#example of for loop
+for i in range(5):
+    print(i) #0 1 2 3 4
+
+#example of while loop
+i = 0
+while i<5:
+    print(i) #0 1 2 3 4
+    i+=1
+
+#example of break statement
+for i in range(5):
+    if i==3:
+        break
+    print(i) #0 1 2
+
+#example of continue statement 
+for i in range(5):
+    if i==3:
+        continue
+    print(i) #0 1 2 4
+
+#example of pass statement
+for i in range(5):
+    if i==3:
+        pass
+    print(i) #0 1 2 3 4
+
+#example of for loop without range()
+for i in [1,2,3,4,5]:
+    print(i) #1 2 3 4 5
+
+#example of for loop with range() and step
+for i in range(1,5,2):
+    print(i) #1 3
+
+#example of for loop with range() and negative step
+for i in range(5,1,-2):
+    print(i) #5 3
+
+#example of list comprehension
+x = [i for i in range(5)]
+print(x) #[0, 1, 2, 3, 4]
+
